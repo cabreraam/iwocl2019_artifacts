27bca549432cb5666441688ef9bd7a9f3f877823
Anthony Cabrera <cabreraam33@gmail.com>
2019-04-08 16:08:05 -0500

    fixed timer.h path

--- code/nw/nw_svm.c
+++ code/nw/nw_svm.c
@@ -25,7 +25,7 @@
 #include <string>
 #include <assert.h>
 #include <CL/cl.h> // don't know if necessary just yet
-#include "../../common/timer.h"
+#include "../common/timer.h"
 #include "../common/opencl_util.h"
 
 // Just HARP
